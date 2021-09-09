da4cfba92d6a27461b61bd361f62b6dd1b289cb7
Eduardo Godoy Fuso <eduardo.godoy@ufms.br>
2021-09-09 18:00:09 -0300

    Subindo projeto para o git

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,121 @@
+﻿using MeuCRUD.Models.Contexto;
+using MeuCRUD.Models.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeuCRUD.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private readonly Contexto _contexto; // Instancia o contexto
+        public UsuariosController(Contexto contexto) // Construtor da classe
+        {
+            _contexto = contexto;
+        }
+
+
+
+        public IActionResult Index() // Utiliza o Index como método LISTAR
+        {
+            var lista = _contexto.Usuario.ToList();
+            CarregaTipoUsuario();
+            return View(lista);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var usuario = new Usuario();
+            CarregaTipoUsuario();
+
+            return View(usuario);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Usuario usuario)
+        {
+            if(ModelState.IsValid) // Validação
+            {
+                _contexto.Usuario.Add(usuario);
+                _contexto.SaveChanges();
+
+                return RedirectToAction("Index"); // Se tudo OK, retorna ao index
+            }
+            CarregaTipoUsuario();
+            return View(usuario); // Se houver informações faltando, retorna para o formulario com as infos anteriormente  inseridas no input
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int Id)
+        {
+            var usuario = _contexto.Usuario.Find(Id);
+           
+            CarregaTipoUsuario();
+            return View(usuario);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _contexto.Usuario.Update(usuario);
+                _contexto.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                CarregaTipoUsuario();
+                return View(usuario);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int Id)
+        {
+            var usuario = _contexto.Usuario.Find(Id);
+            CarregaTipoUsuario();
+            return View(usuario);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Usuario user)
+        {
+            var usuario = _contexto.Usuario.Find(user.Id);
+            if (usuario != null)
+            {
+                _contexto.Usuario.Remove(usuario);
+                _contexto.SaveChanges(); // Salva e faz a alteração do DB
+
+                return RedirectToAction("Index");
+            }
+            return View(usuario);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int Id)
+        {
+            var usuario = _contexto.Usuario.Find(Id);
+            CarregaTipoUsuario();
+            return View(usuario);
+        }
+
+        public void CarregaTipoUsuario()
+        {
+            var ItensTipoUsuario = new List<SelectListItem>
+            {
+                new SelectListItem{ Value = "1", Text = "Administrador"},
+                    new SelectListItem{ Value = "2", Text = "Técnico"},
+                        new SelectListItem{ Value = "3", Text = "Usuário Comum"}
+
+            };
+
+            ViewBag.TiposUsuario = ItensTipoUsuario;
+        }
+    }
+}
---
+++ Models/Contexto/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using MeuCRUD.Models.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeuCRUD.Models.Contexto
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> option) : base(option) // Construtor
+        {
+            Database.EnsureCreated(); // Verifica se o banco existe, caso não exista ele mapeia a entidade e cria as tabelas
+        }
+
+        public DbSet<Usuario> Usuario { get; set; }
+
+    }
+}
